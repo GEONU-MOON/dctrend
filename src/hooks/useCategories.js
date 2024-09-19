cae57c535250e051de54693ed8bc586e563f8045
GEONU-MOON <moondy2209@naver.com>
2024-09-19 10:18:08 +0900

    #21 feat : 카테고리 렌더링 엔드포인트 변경 반영

--- src/hooks/useCategories.js
+++ src/hooks/useCategories.js
@@ -6,8 +6,13 @@ export const useCategories = () => {
 
   useEffect(() => {
     axios
-      .get("https://api.trend.rankify.best/api/v1/news/categories")
+      .get("https://api.trend.rankify.best/api/v1/news/categories/group", {
+        headers: {
+          "X-API-KEY": "AdswKr3yJ5lHkWllQUr6adnY9Q4aoqHh0KfwBeyb14", // API 키 추가
+        },
+      })
       .then((response) => {
+        // console.log("Received categories data:", response.data); // 콘솔에 받은 데이터 출력
         if (response.data.message === "success") {
           setCategories(response.data.data);
         }
